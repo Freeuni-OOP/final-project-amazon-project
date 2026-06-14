5965690e7a16f8c5a3d96dab791b8559df525fc2
mariamiiq <mkoch24@freeuni.edu.ge>
2026-06-14 23:02:15 +0400

    Update ProductRepository.java

--- src/main/java/com/amazon/amazon_backend/repository/ProductRepository.java
+++ src/main/java/com/amazon/amazon_backend/repository/ProductRepository.java
@@ -4,8 +4,17 @@ import com.amazon.amazon_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Integer>{
 
+  List<Product> findByProductNameContainingIgnoreCase(String productName);
+  
+  List<Product> findByCategory_CategoryId(Integer categoryId);
+
+  List<Product> findByCategory_CategoryNameIgnoreCase(String categoryName);
+
+  List<Product> findBySeller_Id(Integer userId);
 }
