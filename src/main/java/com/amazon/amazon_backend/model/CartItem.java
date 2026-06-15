a17d1f59aafbdcefd7885f090965ce6d07003be2
BarbareUgrekhelidze <barbareugrekhelidze32@gmail.com>
2026-06-15 22:35:20 +0400

    added Category tests and some changes to CartItem.java

--- src/main/java/com/amazon/amazon_backend/model/CartItem.java
+++ src/main/java/com/amazon/amazon_backend/model/CartItem.java
@@ -1,4 +1,26 @@
 package com.amazon.amazon_backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Cart_Items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "User_ID", nullable = false)
+    private User user;
+
 }
---
+++ src/test/java/com/amazon/amazon_backend/utilityTest/CategoryConverterTest.java
@@ -0,0 +1,45 @@
+package com.amazon.amazon_backend.utilityTest;
+
+import com.amazon.amazon_backend.dto.CategoryResponse;
+import com.amazon.amazon_backend.model.Category;
+import com.amazon.amazon_backend.utility.CategoryConverter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryConverterTest {
+
+    private CategoryConverter converter;
+
+    @BeforeEach
+    public void setUp(){
+        converter = new CategoryConverter();
+    }
+
+    @Test
+    public void testToCategoryResponseOnTrueCases(){
+        Category category = new Category("House");
+        CategoryResponse response = converter.toCategoryResponse(category);
+
+        assertNotNull(response);
+        assertEquals("House", response.getCategory());
+        assertEquals(category.getCategoryName(), response.getCategory());
+    }
+
+    @Test
+    public void testToCategoryResponseOnFalseCases(){
+        Category category1 = new Category("House");
+        Category category2 = new Category("Sport");
+        Category category3 = new Category("Books");
+
+        CategoryResponse response1 = converter.toCategoryResponse(category1);
+        CategoryResponse response2 = converter.toCategoryResponse(category2);
+        CategoryResponse response3 = converter.toCategoryResponse(category3);
+
+        assertNotEquals(category1.getCategoryName(), response2.getCategory());
+        assertNotEquals(category2.getCategoryName(), response3.getCategory());
+        assertNotEquals(category3.getCategoryName(), response1.getCategory());
+    }
+
+}
