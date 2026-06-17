e157f93969a783553498d89d74f45bc891465ca7
mariamiiq <mkoch24@freeuni.edu.ge>
2026-06-17 14:45:28 +0400

    added product Service and Controller

--- src/main/java/com/amazon/amazon_backend/controller/ProductController.java
+++ src/main/java/com/amazon/amazon_backend/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.amazon.amazon_backend.controller;
 
+import com.amazon.amazon_backend.dto.ProductRequest;
 import com.amazon.amazon_backend.dto.ProductResponse;
+import com.amazon.amazon_backend.dto.ProductUpdateRequests.ImageUpdateRequest;
+import com.amazon.amazon_backend.dto.ProductUpdateRequests.NameDescriptionUpdateRequest;
+import com.amazon.amazon_backend.dto.ProductUpdateRequests.PriceUpdateRequest;
+import com.amazon.amazon_backend.dto.ProductUpdateRequests.QuantityUpdateRequest;
 import com.amazon.amazon_backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -40,5 +45,35 @@ public class ProductController {
         return productService.SearchProductsBySellerId(sellerId);
     }
 
+    @PostMapping
+    public ProductResponse createProduct(@RequestBody ProductRequest request) {
+        return productService.createProduct(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Integer id){
+        productService.deleteProduct(id);
+        return "Product deleted successfully.";
+    }
+
+    @PatchMapping("/{id}/price")
+    public ProductResponse updatePrice(@PathVariable Integer id, @RequestBody PriceUpdateRequest request) {
+        return productService.updatePrice(id, request);
+    }
+
+    @PatchMapping("/{id}/quantity")
+    public ProductResponse updateQuantity(@PathVariable Integer id, @RequestBody QuantityUpdateRequest request) {
+        return productService.updateQuantity(id, request);
+    }
+
+    @PatchMapping("/{id}/image")
+    public ProductResponse updateImage(@PathVariable Integer id, @RequestBody ImageUpdateRequest request) {
+        return productService.updateImage(id, request);
+    }
+
+    @PatchMapping("/{id}/details")
+    public ProductResponse updateNameAndDescription(@PathVariable Integer id, @RequestBody NameDescriptionUpdateRequest request) {
+        return productService.updateNameAndDescription(id, request);
+    }
 
 }
