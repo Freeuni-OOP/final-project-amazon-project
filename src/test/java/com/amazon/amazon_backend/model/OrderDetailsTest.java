bed7d52f3b7e2ede7d2dc6803e6a43053973f27b
lizidata <ldata24@freeuni.edu.ge>
2026-06-18 16:44:47 +0400

    feature: OrderDetails model/entity+repository without tests

--- src/test/java/com/amazon/amazon_backend/model/OrderDetailsTest.java
+++ src/test/java/com/amazon/amazon_backend/model/OrderDetailsTest.java
@@ -19,13 +19,17 @@ class OrderDetailsTest {
         Integer quantity=14;
         BigDecimal amount=new BigDecimal("1978.35");
 
-        OrderDetails orderDetail=new OrderDetails(null, product, order, quantity, amount);
+        Transaction transaction=new Transaction();
+        transaction.setId(3);
+
+        OrderDetails orderDetail=new OrderDetails(null, product, order, quantity, amount, transaction);
 
         assertNull(orderDetail.getOrderDetailID());
         assertEquals(84, orderDetail.getProduct().getProductId());
         assertEquals(9, orderDetail.getOrder().getOrderId());
         assertEquals(14, orderDetail.getQuantity());
         assertEquals(amount, orderDetail.getAmount());
+        assertEquals(3, orderDetail.getTransaction().getId());
     }
 
     @Test
@@ -42,15 +46,20 @@ class OrderDetailsTest {
         orderDetail.setProduct(product);
         orderDetail.setOrder(order);
 
+        Transaction transaction=new Transaction();
+        transaction.setId(3);
+
         Integer quantity=15;
-        BigDecimal amount=new BigDecimal(625.95);
+        BigDecimal amount=new BigDecimal("625.95");
         orderDetail.setQuantity(quantity);
         orderDetail.setAmount(amount);
+        orderDetail.setTransaction(transaction);
 
         assertEquals(67, orderDetail.getOrderDetailID());
         assertEquals(1967, orderDetail.getProduct().getProductId());
         assertEquals(78630, orderDetail.getOrder().getOrderId());
         assertEquals(15, orderDetail.getQuantity());
         assertEquals(amount, orderDetail.getAmount());
+        assertEquals(3, orderDetail.getTransaction().getId());
     }
 }
