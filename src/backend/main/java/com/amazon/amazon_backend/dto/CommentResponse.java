1d13da91384860a0f679a9808e48cac7c2ecd376
nkori24 <nkori@freeuni.edu.ge>
2026-06-23 21:20:22 +0400

    Added notnull and size  annotations to DTO classes

--- src/backend/main/java/com/amazon/amazon_backend/dto/CommentResponse.java
+++ src/backend/main/java/com/amazon/amazon_backend/dto/CommentResponse.java
@@ -1,10 +1,12 @@
 package com.amazon.amazon_backend.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponse {
     private Integer commentId;
     private String commentString;
