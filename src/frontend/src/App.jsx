5186d27a2722c60ea8897997a2c69fc9932df6d7
BarbareUgrekhelidze <barbareugrekhelidze32@gmail.com>
2026-06-27 18:57:48 +0400

    added categories place to select category

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,26 +3,47 @@ import './App.css';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8080/products')
-        .then(data => data.json())
-        .then(fetchedProducts => {
-            setProducts(fetchedProducts);
-        })
-        .catch(() => alert("Products not found"));
+      const fetchInitialData = async () => {
+          try {
+              const [categoryResponse, productsResponse] = await Promise.all([
+                  fetch('http://localhost:8080/categories'),
+                  fetch('http://localhost:8080/products')
+              ]);
+
+              const categoriesData = await categoryResponse.json();
+              const productsData = await productsResponse.json();
+
+              setCategories(categoriesData);
+              setProducts(productsData);
+          } catch (error) {
+              console.error("Error fetching data:", error);
+          }
+      };
+
+      fetchInitialData();
   }, []);
 
   return (
       <div>
         <nav id="navbar">
-          <img src="../public/images/dark-logo.png" alt=""/>
+          <img src="/images/dark-logo.png" alt=""/>
           <input type="text" id="search-item" name="search" placeholder="Search Amazon ..."/>
           <div className="navbar-btns">
             <button className="sign-in">Sign In</button>
             <button className="sign-up">Sign Up</button>
           </div>
         </nav>
+
+        <div id="categories">
+            {categories.map((category) => {
+                return (
+                    <a className="category">{category.categoryName}</a>
+                );
+            })}
+        </div>
 
         <div id="main-box">
           {products.map((product) => {
