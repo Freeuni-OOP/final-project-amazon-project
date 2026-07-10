541eefe7b44e4309ed356d0d2c6fe04b1a19181d
nkori24 <nkori24@freeuni.edu.ge>
2026-07-10 15:11:51 +0400

    Fix: fixed imports in ReviewRequest.java

--- src/backend/main/java/com/amazon/amazon_backend/dto/ReviewRequest.java
+++ src/backend/main/java/com/amazon/amazon_backend/dto/ReviewRequest.java
@@ -1,6 +1,8 @@
 package com.amazon.amazon_backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
